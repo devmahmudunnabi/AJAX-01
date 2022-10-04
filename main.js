a12aff08d7e0ab6bcef11e61f18e35904d11636d
shaykatWorks <shaykatworks@gmail.com>
2022-10-05 02:08:24 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function(){
+    // jQuery("#btn1").click(function(){
+    //     forinsart();
+    // });
+    // jQuery("#btn2").click(function(){
+    //     fordelete();
+    // });
+    // jQuery("#btn3").click(function(){
+    //     forupdate();
+
+    // });
+
+    // function forinsart(){
+    //     var data= "insert";
+    //     $.ajax({
+    //         url:"process.php",
+    //         type:'post',
+    //         data:{
+    //             shykat: data
+    //         },
+    //         success:function(response){
+    //             jQuery("p").text(response);
+    //         }
+    //     });
+    // }
+
+    // function fordelete(){ 
+    //     var data = "delete";
+    //     $.ajax({
+    //         url:"process.php",
+    //         type: 'post',
+    //         data :{
+    //             shykat: data
+    //         },
+    //         success:function(response){
+    //             jQuery("p").text(response);
+    //         }
+    //     });
+    // }
+    // function forupdate(){
+    //     var data = "update";
+    //     $.ajax({
+    //         url:"process.php",
+    //         type:'post',
+    //         data:{
+    //             shykat:data
+    //         },
+    //         success:function(response){
+    //             jQuery("p").text(response);
+    //         }
+            
+    //     });
+    // }
+
+
+
+
+
+
+
+
+});
---
+++ process.php
@@ -0,0 +1,15 @@
+<?php
+//  $data =$_POST["shykat"];
+//  $data();
+//     function insert(){
+//         echo "This is insert section ";
+//     }
+//     function delete(){
+//         echo "This is delete section";
+    
+//     }
+//     function update(){
+//         echo "This is update";
+//     }
+
+?>
